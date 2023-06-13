36ef5d0de0e0224c4c4f663efb6e5d2f4ce50beb
moskvSergey <116438604+moskvSergey@users.noreply.github.com>
2023-06-14 00:09:41 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 from datetime import datetime as dt
 import requests
 import discord
+import unicodedata
 from discord.ext import commands
 import ffmpeg
 import os
@@ -10,7 +11,7 @@ import random
 
 
 token = '4eeb56814b635a2b47d96771d700fb97'
-dis_token = 'MTExMjM0MzI4Nzc0ODEwMDEzOA.GrgCZ9.dCcCfKWF8lal7wCwQRFYj60C8jG0I7FPaMTKrs'
+dis_token = 'MTExMjM0MzI4Nzc0ODEwMDEzOA.GHXqXu.flJ1xD-svcf8g2DvV9wKyXLcryGyHu0leq6Mq0'
 
 
 async def get_weather(city):
@@ -87,6 +88,8 @@ async def short_message(ctx, answer, view=None):
 
 
 
+
+
 @bot.event
 async def on_command_error(ctx, error):
     print(error)
@@ -103,10 +106,16 @@ async def weather(ctx, city):
     await short_message(ctx, answer="", view=view)
 
 
+
+def replace_non_ascii(text):
+    return ''.join(c if ord(c) < 128 else unicodedata.normalize('NFKD', c).encode('ascii', 'ignore').decode('ascii') for c in text)
+
+
 async def write_log(ctx, answer):
     member = ctx.message.author.display_name
     with open(f"{member}.txt", "a") as f:
-        f.write(f"{ctx.message.content}\n{answer}\n{dt.now()}\n\n")
+        f.write(replace_non_ascii(f"{ctx.message.content}\n{answer}\n{dt.now()}\n\n"))
+
 
 
 @bot.command(name="Закат", brief="     Shows time untill sunset")
@@ -172,22 +181,35 @@ async def infinite_loop(ctx, voice, songs):
                         executable="ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe",
                         source=song))
                 await short_message(ctx, f"Playing {song}..")
+
                 break
             else:
                 await asyncio.sleep(1)
 
-    await infinite_loop()
     await short_message(ctx, "Finished playing all songs.")
+    await voice.disconnect()
 
 
 async def play(ctx, name):
+    global stop_song
+    if ctx.voice_client is not None:
+        stop_song = True
+        await ctx.voice_client.disconnect()
     channel = ctx.author.voice.channel
+    if channel is None:
+        await ctx.send("Вы не подключены к голосовому каналу")
+        return
     voice = await channel.connect()
 
-    if name == "": name = ctx.message.author.display_name
 
+    if name == "": name = ctx.message.author.display_name
     folder_path = f"music/{name}/"
-    songs = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".mp3")]
+    try:
+        songs = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".mp3")]
+    except:
+        await short_message(ctx, f"У {name} еще нет музыки.")
+        folder_path = f"music/K0TtiNk斯蒂勒/"
+        songs = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".mp3")]
     return voice, songs
 
 
